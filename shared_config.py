9499d02dfea2b6ab2cc5236e8f69ee10fcffda73
IAN ZHU <zhuy17@rose-hulman.edu>
2025-05-23 12:49:19 -0400

    Add Shared_config.py

--- shared_config.py
+++ shared_config.py
@@ -3,4 +3,4 @@ HOST = "YOUR_REMOTE_SERVER_IP"  # Replace with your remote server's IP
 PORT = 5050                     # Make sure this port is open on your remote server
 
 # Security
-ENCRYPTION_KEY = b'your-32-byte-encryption-key-here!!'  # Change this to a secure key
+ENCRYPTION_KEY = b'your-32-byte-encryption-key-here!'  # Change this to a secure key
